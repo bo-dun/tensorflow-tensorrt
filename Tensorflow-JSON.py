a5cb354219f339212bac6aa3f5dfbe8289c0ea39
Peter Dun <bodun@stanford.edu>
2017-08-21 11:21:10 -0400

    Update Tensorflow-JSON.py

--- Tensorflow-JSON.py
+++ Tensorflow-JSON.py
@@ -1,85 +1,62 @@
-import tensorflow
 import numpy as np
-import re
 import json
 
 
 prefixes = ['softmax', 'fc', 'conv', 'max_pool', 'avg_pool', 'relu'] # ADD CONCAT
 
-
-def parse_stride(prefix, name):
-    name = name[len(prefix):]
-    m1 = re.match('_\d+_\d+', name)
-    if m1:
-        m2 = re.findall('\d+', m1.group(0))
-        return int(m2[0]), int(m2[1])
-
-    return -1, -1
-
-
-def parse_window_and_stride(prefix, name):
-    name = name[len(prefix):]
-    m1 = re.match('_\d+_\d+_\d+_\d+', name)
-    if m1:
-        m2 = re.findall('\d+', m1.group(0))
-        return int(m2[0]), int(m2[1]), int(m2[2]), int(m2[3])
-
-    return -1, -1, -1, -1
-
-
 def validate_prefixes(names):
     for name in names:
-        if name.startswith(prefixes[0]) or name.startswith(prefixes[1]) or name.startswith(prefixes[5]):
-            continue
-        elif name.startswith(prefixes[2]):
-            stride = parse_stride(prefixes[2], name)
-            if stride != -1:
-                continue
-        elif name.startswith(prefixes[3]):
-            window = parse_window_and_stride(prefixes[3], name)
-            if window != -1:
-                continue
-        elif name.startswith(prefixes[4]):
-            window = parse_window_and_stride(prefixes[4], name)
-            if window != -1:
-                continue
+        index = name.rfind('/')
+        if index != -1: section = name[index + 1:]
+        else: section = name
+        hasPrefix = False
+        for prefix in prefixes:
+            if (section.startswith(prefix)):
+                hasPrefix = True
+                break
 
-        return False
+        if not hasPrefix:
+            return False
 
     return True
 
+# Prefix of the namespaces in the dictionary
+prefix = '/home/peter/Desktop/'
 
-def count(weight):
-    if type(weight) == list:
-        return sum(count(subitem) for subitem in weight)
-    else:
-        return 1
-
-
-# Max pool must be named in the form 'max_pool_{{windowHeight}}_{{windowWidth}}_{{strideHeight}}_{{strideWidth}}' + etc.
-# Average pool must be named in the form 'average_pool_{{windowHeight}}_{{windowWidth}}_{{strideHeight}}_{{strideWidth}}' + etc.
+# Max pool must have entry in dict mapped to a list of the format [windowHeight, windowWidth, strideHeight, strideWidth]
+    # Also must be named 'max_pool' + etc.
+# Average pool must have entry in dict mapped to a list of the format [windowHeight, windowWidth, strideHeight, strideWidth].
+    # Also must be named 'avg_pool' + etc.
 # Softmax must be named 'softmax' + etc.
 # Full connected must be named 'fc' + etc.
-# Convolutional must be named in the form 'conv_{{strideHeight}}_{{strideWidth}}' + etc.
-
-
-arrangements = [[0,1,2,3],[0,1,3,2],[0,2,1,3],[0,2,3,1],[0,3,1,2],[0,3,2,1],[1,0,2,3],[1,0,3,2],[1,2,0,3],[1,2,3,0],
-                [1,3,0,2],[1,3,2,0],[2,0,1,3],[2,0,3,1],[2,1,0,3],[2,1,3,0],[2,3,0,1],[2,3,1,0],[3,0,1,2],[3,0,2,1],[3,1,0,2],[3,1,2,0],[3,2,0,1],[3,2,1,0]]
-# layer['weights_hwio'] = np.transpose(weight, (3, 2, 0, 1)).tolist()   # Rearrange order to be compatible with TensorRT
-def convert(graph, namescopes, session, channels, height, width):
+# Convolutional layer must have entry in dict mapped to a list of the format [strideHeight, strideWidth, padding]
+    # Padding is an optional entry for if you want custom padding, not 'SAME' padding,
+def convert(graph, namescopes, dict, session, channels, height, width):
     if not validate_prefixes(namescopes):
         return None
+    # Create a model specification file named "input" that specifies input tensor parameters
     json_object = {}
-    json_object['layers'] = []
     json_object['num_input_channels'] = channels
     json_object['input_height'] = height
     json_object['input_width'] = width
+    with open(prefix + 'input', 'w') as outfile:
+        json.dump(json_object, outfile)
+        outfile.close()
+    counter = 0
+
+    # Create a model specification file for each layer in the network
     for namescope in namescopes:
-        print(namescope)
+        counter += 1
+        index = namescope.rfind('/')
+        if index != -1: section = namescope[index + 1:]
+        else: section = namescope
+        print(section)
         layer = {}
-        if namescope.startswith(prefixes[0]):
+        if section.startswith(prefixes[0]):
+            # If layer is softmax
             layer['name'] = 'softmax'
-        elif namescope.startswith(prefixes[1]):
+        elif section.startswith(prefixes[1]) and namescope not in dict:
+            # If layer is fully connected
             layer['name'] = 'fc'
             for variable in graph.get_collection('trainable_variables', namescope):
                 name = variable.name[len(namescope) + 1:]
@@ -90,7 +67,8 @@ def convert(graph, namescopes, session, channels, height, width):
                     bias = session.run(variable)
                     layer['biases'] = bias.tolist()
                     layer['num_outputs'] = len(bias)
-        elif namescope.startswith(prefixes[2]):
+        elif section.startswith(prefixes[2]) or (namescope in dict and (len(dict[namescope]) == 2 or len(dict[namescope]) == 3)):
+            # If layer is convolutional
             layer['name'] = 'conv'
             for variable in graph.get_collection('trainable_variables', namescope):
                 name = variable.name[len(namescope) + 1:]
@@ -105,19 +83,32 @@ def convert(graph, namescopes, session, channels, height, width):
                     bias = session.run(variable)
                     layer['biases'] = bias.tolist()
                     layer['num_outputs'] = len(bias)
-            layer['stride_height'], layer['stride_width'] = parse_stride(prefixes[2], namescope)
-        elif namescope.startswith(prefixes[3]):
+            properties = dict[namescope]
+            layer['stride_height'] = properties[0]
+            layer['stride_width'] = properties[1]
+            if (len(properties) == 3): layer['padding'] = properties[2]
+            else: layer['padding'] = -1
+            print(layer['padding'])
+        elif section.startswith(prefixes[3]):
+            # If layer is max pool
             layer['name'] = 'max_pool'
-            layer['window_height'], layer['window_width'], layer['stride_height'], layer['stride_width']\
-                = parse_window_and_stride(prefixes[3], namescope)
-        elif namescope.startswith(prefixes[4]):
+            properties = dict[namescope]
+            layer['window_height'] = properties[0]
+            layer['window_width'] = properties[1]
+            layer['stride_height'] = properties[2]
+            layer['stride_width'] = properties[3]
+        elif section.startswith(prefixes[4]):
+            # If layer is average pool
             layer['name'] = 'avg_pool'
-            layer['window_height'], layer['window_width'], layer['stride_height'], layer['stride_width']\
-                = parse_window_and_stride(prefixes[4], namescope)
-        elif namescope.startswith(prefixes[5]):
+            properties = dict[namescope]
+            layer['window_height'] = properties[0]
+            layer['window_width'] = properties[1]
+            layer['stride_height'] = properties[2]
+            layer['stride_width'] = properties[3]
+        elif section.startswith(prefixes[5]):
+            # If layer is a ReLU activation
             layer['name'] = 'relu'
 
-        json_object['layers'].append(layer)
-
-        with open('/home/peter/Desktop/final_mnist.txt', 'w') as outfile:
-            json.dump(json_object, outfile)
+        with open(prefix + str(counter), 'w') as outfile:
+            json.dump(layer, outfile)
+            outfile.close()
